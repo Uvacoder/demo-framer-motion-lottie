43279e099b1164ac4990a69eebbfd2cdd4cfab9f
Alex <abergsf2017@gmail.com>
2021-09-05 16:56:05 -0700

    Updates

---
+++ precache-manifest.d4421dda1e4661a5258c531dd4c4fb25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26f2c7ed9f95585c8de0",
+    "url": "/framer-menu-lottie/static/css/main.a3cdceb6.chunk.css"
+  },
+  {
+    "revision": "26f2c7ed9f95585c8de0",
+    "url": "/framer-menu-lottie/static/js/main.26f2c7ed.chunk.js"
+  },
+  {
+    "revision": "bd27f14a15f6ec30469d",
+    "url": "/framer-menu-lottie/static/js/1.bd27f14a.chunk.js"
+  },
+  {
+    "revision": "4f7bd8b90a910abd03fa",
+    "url": "/framer-menu-lottie/static/js/runtime~main.4f7bd8b9.js"
+  },
+  {
+    "revision": "7413e80e97abfecfc529fe803b12ee1b",
+    "url": "/framer-menu-lottie/index.html"
+  }
+];
